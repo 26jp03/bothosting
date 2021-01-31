0672c3f188c66199c7709ec6b8b25b659ee0b501
26jp03 <59638944+26jp03@users.noreply.github.com>
2021-01-31 18:50:14 +0900

    Update hellobot.py

--- hellobot.py
+++ hellobot.py
@@ -27,5 +27,5 @@ async def on_message(message):
         await message.channel.send(msg)
         
         
-access_token = os.environ["BOT_TOKEN"]
+access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
