beb60b431adf6b7b652a2ebbe7b04fbf9d9181f7
26jp03 <59638944+26jp03@users.noreply.github.com>
2021-02-03 14:56:18 +0900

    Update hellobot.py

--- hellobot.py
+++ hellobot.py
@@ -26,6 +26,14 @@ async def on_message(message):
         msg = '{0.author.mention} 어서오세요 #🔔규칙방🔔 봐주시고 #💬수다방💬 에서 소통하세요'.format(message)
         await message.channel.send(msg)
         
+    if message.content.startswith("ㅂ/사진목록"):
+        one = "빵신사진"
+        two = "바보"
+        await message.channel.send(str(one)+'과 '+str(two))
+    
+    if message.content.startswith("ㅂ/사진"):
+        pic = message.content.split(" ")[1]
+        await message.channel.send(file=discord.File(pic))        
         
 access_token = os.environ['BOT_TOKEN']
 client.run(access_token)
